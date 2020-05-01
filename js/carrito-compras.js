617d01e292343cdbc177fb8e0b19ee76bd0060d4
damian212121 <nico.damian.ex@gmail.com>
2020-05-01 01:55:40 -0300

    Se agrego carrito de compra

---
+++ js/carrito-compras.js
@@ -0,0 +1,38 @@
+localStorage.setItem('ver', 'inline-block');
+
+$(document).ready(function () {
+    $('#montoId').text(localStorage.getItem('monto'));
+
+    $('article').click(function () {
+        let monto = localStorage.getItem('monto');
+        if (!monto) {
+            localStorage.setItem('monto', 0);
+            monto = localStorage.getItem('monto');
+        }
+        if ($(this).hasClass('seccion--item-selecionado')) {
+            $(this).removeClass('seccion--item-selecionado');
+            localStorage.setItem('monto', Number.parseFloat(monto) - 1000);
+        } else {
+            $(this).addClass('seccion--item-selecionado');
+            localStorage.setItem('monto', Number.parseFloat(monto) + 1000);
+        }
+    });
+});
+
+$(window).bind('storage', function (e) {
+    $('#montoId').text(localStorage.getItem('monto'));
+    $('.Carrito').css('display', localStorage.getItem('ver'));
+
+    console.log(e.originalEvent.key, e.originalEvent.newValue);
+});
+$(document).on('scroll', function () {
+    var desplazamientoActual = $(document).scrollTop();
+    console.log(desplazamientoActual);
+    var controlArriba = $('.Carrito');
+    if (desplazamientoActual > 150) {
+        controlArriba.addClass('Carrito-top');
+    }
+    if (desplazamientoActual < 100) {
+        controlArriba.removeClass('Carrito-top');
+    }
+});
--- js/validar-form.js
+++ js/validar-form.js
@@ -1,85 +1,87 @@
-(function(){
-var formulario= document.form,
-elementos= formulario.elements,
-boton=document.getElementById('enviar');
-var valnombre= function(e){
-    if(formulario.nombre.value == 0){
-        formulario.email.className= formulario.email.className + ' invalido';
-        alert("el campo nombre está vacio");
-        e.preventDefault();
+localStorage.setItem('ver', 'none');
+$(document).ready(function () {
+    let form = $('#form');
+    form.find('input ,select, div').each(function () {
+        $(this).blur(function () {
+            $(this).removeClass('invalido');
+        });
+    });
+    $('#enviar').click(() => {
+        let datos = [];
+        let flags = true;
+        form.find('input ,select ,textarea').each(function () {
+            if ((this.name !== 'comentario' && !this.value) || this.value === '0') {
+                $(this).addClass('invalido');
+                alert('Los Siguientes campos son obligatorios ' + this.name);
+                flags = false;
+            } else if (this.name === 'email' && valEmail(this.value)) {
+                $("#form > input[type='email']").addClass('invalido');
+                alert('El email ingresado es invalido');
+                flags = false;
+            } else if (
+                this.name === 'sexo' &&
+                !$("input[name='sexo']:radio").is(':checked') &&
+                !$('#radioGenero').hasClass('invalido')
+            ) {
+                $('#radioGenero').addClass('invalido');
+                alert('Debe seleccionar Genero');
+                flags = false;
+            }
+            if (flags) {
+                datos.push(this);
 
+                if (this.name === 'sexo' && !this.checked) {
+                    datos.pop();
+                }
+            } else {
+                datos = [];
+            }
+        });
+        let myDatos = {};
+        if (datos.length > 0) {
+            datos.map((elem) => {
+                if (elem.name === 'valoracion') {
+                    myDatos[elem.name] = elem.options[elem.value].innerText;
+                } else {
+                    myDatos[elem.name] = elem.value;
+                }
+            });
+
+            alert(JSON.stringify(myDatos));
+        }
+    });
+    $("input[name='sexo']:radio").click(() => {
+        $('#radioGenero').removeClass('invalido');
+    });
+
+    $("#form > input[type='text']").keypress(function ({ charCode, which }) {
+        var regex = new RegExp('^[a-zA-Z ]+$');
+        var key = String.fromCharCode(!charCode ? which : charCode);
+        if (!regex.test(key)) {
+            event.preventDefault();
+            return false;
+        }
+    });
+
+    $("#form > input[type='email']").blur(function ({ target: { value } }) {
+        if (valEmail(value)) {
+            $(this).addClass('invalido');
+            alert('El email ingresado es invalido');
+        }
+    });
+
+    $('#idCancelar').click(() => {
+        var opcion = confirm('Desea cancelar la operacion');
+        if (opcion) {
+            window.history.back();
+        }
+    });
+});
+
+function valEmail(email) {
+    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    if (!emailReg.test(email)) {
+        return true;
     }
-};
-
-var valapellido= function(e){
-    if(formulario.apellido.value == 0){
-        formulario.email.className= formulario.apellido.className + ' invalido';
-        alert("el campo apellido está vacio");
-        e.preventDefault();
-    }
-};
-
-var valemail=function(e){
-    if( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(formulario.email.value)) ) {
-        alert("ingrese una direccion de mail por favor");
-        formulario.email.className= formulario.email.className + ' invalido';
-        e.preventDefault();
-      }
-      else{
-          formulario.email.className= formulario.email.className.replace(" invalido", "");
-      }
-} 
-
-
-var valdate= function(e){
-    var RegExPattern = /^\d{1,2}\/\d{1,2}\/\d{2,4}$/;
-    if ((!formulario.fecha.value.match(RegExPattern)) && (formulario.fecha.value="")) {
-        alert("ingrese un formato de fecha correcto");
-        formulario.fecha.className= formulario.fecha.className + ' invalido';
-        e.preventDefault();
-        
-          
-    } else {
-        formulario.fecha.className= formulario.fecha.className.replace(" invalido", "");
-        
-    }
-};
-
-var valSelect= function(e){
-    if(formulario.valoracion.value== 0)
-          {
-           alert("Selecciona Una opción");
-           formulario.valoracion.className= formulario.valoracion.className +' invalido';
-           e.preventDefault();
-           
-          }
-    else{
-        formulario.valoracion.className= formulario.valoracion.className.replace("");
-
-    }
-
-};
-
-var validarSexo= function(e){
-    if(formulario.sexo[0].checked==true || formulario.sexo[1].checked==true){
-    }
-    else{
-        alert("elije algun sexo");
-        e.preventDefault();
-        
-    }
-    
 }
-
-
-var validarForm=function(e){
-    valnombre(e);
-    valapellido(e);
-    valemail(e);
-    valdate(e);
-    valSelect(e);
-    validarSexo(e);
-};
-
-formulario.addEventListener("submit", validarForm);
-}());
+$('.Carrito').css('display', 'none');
