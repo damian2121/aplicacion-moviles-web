8b362292ef6b03625ab04e7e1265a49e3fc71785
damian212121 <nico.damian.ex@gmail.com>
2020-05-01 21:26:14 -0300

    master :se cambia por https

--- js/objeto.js
+++ js/objeto.js
@@ -1,19 +1,24 @@
-const urlPelotas = "http://www.json-generator.com/api/json/get/bIOVfDVzaW?indent=2", urlCamisetas = "http://www.json-generator.com/api/json/get/cfdzqUUeEO?indent=2",
-urlZapatillas = "http://www.json-generator.com/api/json/get/cuSbIljKhu?indent=2", urlAros = "http://www.json-generator.com/api/json/get/bIulZbDUya?indent=2";
+const urlPelotas = 'https://www.json-generator.com/api/json/get/bIOVfDVzaW?indent=2',
+    urlCamisetas = 'https://www.json-generator.com/api/json/get/cfdzqUUeEO?indent=2',
+    urlZapatillas = 'https://www.json-generator.com/api/json/get/cuSbIljKhu?indent=2',
+    urlAros = 'https://www.json-generator.com/api/json/get/bIulZbDUya?indent=2';
 
-function mostrarObjeto(seccion, descripcion, url){
-    var formPrincipal = $("#formPrincipal");
+function mostrarObjeto(seccion, descripcion, url) {
+    var formPrincipal = $('#formPrincipal');
     $.get(url, formPrincipal.serialize())
-    .done(function(response){
-        $.each(response, function(i,item) {
-            formPrincipal.find(seccion + " .seccion--container").append(
-                $("<article/>").addClass("seccion--item").append(   
-                    $("<img/>").css({"background-image" : "url(" + item.img + ")"}),
-                    $("<figcaption/>").text(item.titulo)
-                )
-            )
+        .done(function (response) {
+            $.each(response, function (i, item) {
+                formPrincipal.find(seccion + ' .seccion--container').append(
+                    $('<article/>')
+                        .addClass('seccion--item')
+                        .append(
+                            $('<img/>').css({ 'background-image': 'url(' + item.img + ')' }),
+                            $('<figcaption/>').text(item.titulo)
+                        )
+                );
+            });
+        })
+        .fail(function () {
+            alert('Error al buscar: ' + descripcion);
         });
-    }).fail(function(){
-        alert("Error al buscar: " + descripcion );
-    });
 }
